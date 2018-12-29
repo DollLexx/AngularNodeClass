16849bf7682a958a792df434074dd05afde89563
Deb Sutter <debthefirebreather@gmail.com>
2018-12-29 17:42:52 -0500

    Added Mondo DB
    added Mongo DB and ability to Add/Delete from database from UI

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -26,4 +26,7 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.postsSubscription.unsubscribe();
   }
 
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
 }
